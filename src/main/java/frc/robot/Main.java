4f4b6fe87ddba52e45205b188721c476d67cc1f9
1422NeonKnights <1422neonknights@gmail.com>
2022-01-17 10:32:54 -0800

    kickoff

--- src/main/java/frc/robot/Main.java
+++ src/main/java/frc/robot/Main.java
@@ -20,6 +20,7 @@ public final class Main {
    * <p>If you change your main robot class, change the parameter type.
    */
   public static void main(String... args) {
+    System.out.println("hello");
     RobotBase.startRobot(Robot::new);
   }
 }
