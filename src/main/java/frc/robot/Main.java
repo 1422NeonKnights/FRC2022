fa43a8d5d5d77c6ad252ef70ee09a141e01f117b
1422NeonKnights <1422neonknights@gmail.com>
2022-01-31 15:46:15 -0800

    changed "hi" ->"bye"

--- src/main/java/frc/robot/Main.java
+++ src/main/java/frc/robot/Main.java
@@ -20,7 +20,7 @@ public final class Main {
    * <p>If you change your main robot class, change the parameter type.
    */
   public static void main(String... args) {
-    System.out.println("hi");
+    System.out.println("bye");
     RobotBase.startRobot(Robot::new);
    
   }
